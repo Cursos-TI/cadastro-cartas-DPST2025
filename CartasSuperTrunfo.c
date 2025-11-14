04923c033963181ab4e730e505e61fa98ba1b1f0
DPST2025 <dsilvateixeira81@gmail.com>
2025-11-14 14:32:40 +0000

    declarando as variaveis de letra do estado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,22 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+  printf("Desafio Novato Super Trunfo!\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1[1];
+  char estado2[1];
+  char estado3[1];
+  char estado4[1];
+  char estado5[1];
+  char estado6[1];
+  char estado7[1];
+  char estado8[1];
+  
+
+
+
+
+  
 
   // Área para entrada de dados
 
